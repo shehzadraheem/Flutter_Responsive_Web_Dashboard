bc40ad592259d1c7900b5cb6f03e90b2c19542f1
shehzad Raheem <shehzadraheem.sr38@gmail.com>
2022-07-15 23:10:55 +0500

    first commit

---
+++ lib/views/dashboard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
